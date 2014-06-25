956d9ad2a2851755cc1b92617c886941e5e1d7e2
FredrikGhofran <fredrik_ghofran@hotmail.com>
2014-06-25 10:47:32 +0200

    Ghofran är lagom duktig

--- js/jbff.js
+++ js/jbff.js
@@ -1,18 +1,38 @@
 // The models
 // == == == == == == == == == == == == == == == == == == == == == == == == ==
 
+
+Date.prototype.format = function(format){ //author: meizz 
+var o = { "M+" : this.getMonth()+1, //month 
+"d+" : this.getDate(), //day 
+"h+" : this.getHours(), //hour
+ "m+" : this.getMinutes(), //minute
+  "s+" : this.getSeconds(), //second 
+  "q+" : Math.floor((this.getMonth()+3)/3), //quarter
+   "S" : this.getMilliseconds() //millisecond
+    }; 
+    if(/(y+)/.test(format)) 
+    	format=format.replace(RegExp.$1, (this.getFullYear()+"").substr(4 - RegExp.$1.length)); 
+    for(var k in o) if(new RegExp("("+ k +")").test(format)) 
+    	format = format.replace(RegExp.$1, RegExp.$1.length==1 ? o[k] : ("00"+ o[k]).substr((""+ o[k]).length));
+     return format;
+
+      };
+
+
 Article = Backbone.Model.extend({
  	defaults:function(){
 
  		return{ 
 	 		title   : "",
 	 		text    : "",
+	 		introductionText : "",
 	 		author  : "",
 	 		category: "",
 	 		imageURL: "",
 	 		datePublished: "",
 	 		lastEdit: "",
-	 		comments: ["", ""]
+	 		comments: []
  		};
 	}
 });
@@ -36,12 +56,17 @@ ArticlesView = Backbone.View.extend({
 
 	initialize: function() {
 
-		this.listenTo(this.collection, "change add remove", this.render);
 		this.collection.on('add remove change',this.render,this);
 		this.render();
 	},
 
 	render: function() {
+		console.log("articles view render");
+		if(myRoute.routes[Backbone.history.fragment] == "start"){
+			$(".adminButtons").html("");
+		}
+
+		
 		if( this.collection !== undefined ){
 			this.$el.html( this.template( {articles: this.collection.toJSON()} ) );
 		}
@@ -61,13 +86,26 @@ ArticleView = Backbone.View.extend({
 	comment:function(){
 		var myName = $('#commentName').val();
 		var myText = $('#commentText').val();
-		var comment = this.model.get('comments');
 
-		comment.set({name:myName,text:myText});
-		this.model.push({comments:comment});
+		var nameWithoutSpace = myName.replace(/\s/g, '');
+		var textWithoutSpace = myText.replace(/\s/g, '');
 
+		if(nameWithoutSpace == "" || textWithoutSpace == ""){
+				alert("Fill in all fields");
+		}else{
+					var comment = this.model.get('comments').slice(0);
+
+					
+					comment.push({name:myName, text:myText, timestamp:new Date().format("dd/MM h:mm") });
+		
+		
+		this.model.set({sync: "sync"});//DETTA SKRIVER VI BARA FÖR ATT SYNKA MED DATABASEN
+		this.model.set({comments:comment});
 		$('#commentName').val("");
 		$('#commentText').val("");
+		}
+
+
 	},
 	initialize: function() {
 
@@ -77,6 +115,15 @@ ArticleView = Backbone.View.extend({
 
 	render: function() {
 		if( this.model !== undefined ){
+					console.log("article view render");
+
+			// Kod för att visa redigeringsknappar...
+
+			//if(admin){} kör koden under
+			var editField = Handlebars.compile( $("#edit-article").html());
+
+			$(".adminButtons").html( editField() );
+
 			this.$el.html(this.template(this.model.toJSON()));
 		}
 	}
@@ -93,6 +140,7 @@ AddArticleView = Backbone.View.extend({
 
 	render: function() {
 			this.$el.html(this.template());
+			$(".adminButtons").html("");
 	},
 	events: {
 		"click button":"save",
@@ -100,10 +148,23 @@ AddArticleView = Backbone.View.extend({
 	save:function(){
 		var myText = $('#text').val();
 		var myTitle = $('#title').val();
-		var article = new Article({title:myTitle,text:myText});
+		var myIntroductionText = $('#introductionText').val();
+
+		var introWithoutSpace = myIntroductionText.replace(/\s/g, '');
+		var titleWithoutSpace = myTitle.replace(/\s/g, '');
+		var textWithoutSpace = myText.replace(/\s/g, '');
+
+		if(titleWithoutSpace == "" || textWithoutSpace == "" || introWithoutSpace == ""){
+				alert("Fill in all fields");
+		}else{
+
+		var article = new Article({title:myTitle,text:myText,introductionText:myIntroductionText,datePublished:new Date().format("dd/MM h:mm")});
 
 		articles.add(article);
 		router.navigate('',{trigger:true});
+		}
+
+
 	}
 
 });
@@ -119,10 +180,8 @@ Router = Backbone.Router.extend({
 		'add-article':'add'
 	},
 	start:function(){
-				//var article = new Article({title:"123",text:"333",comments:[{name:"fred",text:"hej"},{name:"fred2",text:"hej2"}]});
 
 		var articlesView = new ArticlesView({ collection: articles });
-		//articles.add(article);
 		$(".content").html(articlesView.el);
 	},
 	view:function(id){
@@ -141,7 +200,7 @@ Router = Backbone.Router.extend({
 	}
 });
 
-window.router = new Router();
+var myRoute = window.router = new Router();
 
 Backbone.history.start();
 
